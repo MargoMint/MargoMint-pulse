1787c7dd77dd9d1645c56eaabc54c74b96cab26b
Rita <minti_gin@op.pl>
2024-03-12 16:36:35 +0100

    finished the thread with the addition of a Tiny slider
    (I won't use this option)

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});

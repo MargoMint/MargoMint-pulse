22e5e3f0fb93409db17554dde3b10c2e94c7d683
Rita <minti_gin@op.pl>
2024-03-25 17:12:23 +0100

    add php mailer
    not worked

--- src/js/script.js
+++ src/js/script.js
@@ -81,4 +81,25 @@ $(document).ready(function(){
     valideForms('#order form');
 
     $('input[name=phone]').inputmask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });

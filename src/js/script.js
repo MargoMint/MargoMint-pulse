d5af2c865275f216e24b591d7cd2a1dd23ad534d
Rita <minti_gin@op.pl>
2024-03-25 13:26:03 +0100

    moved the index.html to the src folder

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1500,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../src/icons/chevron-left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../src/icons/chevron-right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,

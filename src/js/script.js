5e6e3b6b556422fe7a16bdaa11dabc1aeea6dbc0
Rita <minti_gin@op.pl>
2024-03-26 14:20:35 +0100

    add animation in "advantages icons"

--- src/js/script.js
+++ src/js/script.js
@@ -102,4 +102,25 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    //page up (smooth scroll)
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1100, function(){
+            window.location.hash = hash;
+        });
+        }
+    });
 });
